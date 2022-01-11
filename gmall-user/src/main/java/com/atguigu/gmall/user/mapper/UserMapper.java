7d076b3d450634bc1142ed572d0161fd3d0566cd
gxd <1670884273@qq.com>
2022-01-11 13:33:47 +0800

    day01

---
+++ gmall-user/src/main/java/com/atguigu/gmall/user/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.atguigu.gmall.user.mapper;
+
+import com.atguigu.gmall.user.bean.UmsMember;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper extends tk.mybatis.mapper.common.Mapper<UmsMember> {
+    List<UmsMember> selectAllUser();
+}
